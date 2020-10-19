d8bcb1e3a42ea2842258cee18551f8f97f954623
052hide <hidetoshi1990@gmail.com>
2020-10-19 20:17:51 +0900

    create nuxt app

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class LayoutDefault extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class PageIndex extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
